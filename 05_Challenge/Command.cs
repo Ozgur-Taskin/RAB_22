11d1f46a22873407011eadafb4b34ae562cb9703
Ozgur Taskin <mozgurtaskin@yahoo.com>
2022-12-12 17:25:22 +0000

    Final

--- 05_Challenge/Command.cs
+++ 05_Challenge/Command.cs
@@ -2,11 +2,14 @@
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
 
 #endregion
 
@@ -25,34 +28,95 @@ namespace _05_Challenge
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            //insert families to room location point
+            //filter rooms
+            FilteredElementCollector col = new FilteredElementCollector(doc);
+            col.OfCategory(BuiltInCategory.OST_Rooms);
 
-            Selection sel = uidoc.Selection;
+            //get furniture set
+            List<string[]> furnitureSetList = Utils.GetFurnitureSets();
 
-            // Retrieve elements from database
+            //get furniture types
+            List<string[]> furnitureTypesList = Utils.GetFurnitureTypes();
 
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
+            //create a list for the list items
+            string furnitureSetListItems = "";
 
-            // Filtered element collector is iterable
+            //create a string array for funitures
+            string[] tempFurnitureListArray;
+            List<string>  furnitureList = new List<string>();
 
-            foreach (Element e in col)
+            //create variables for families
+            string revFamName = "";
+            string revFamType = "";
+
+            //transaction
+            using (Transaction tran = new Transaction(doc))
             {
-                Debug.Print(e.Name);
+                tran.Start("Insert Family");
+
+
+                //get all the room location poins
+                foreach(SpatialElement room in col)
+                {
+                    //get room location
+                    Location loc = room.Location;
+                    //get location point
+                    LocationPoint locPoint = loc as LocationPoint;
+                    //assign location point value to a variable to use later
+                    XYZ roomPoint = locPoint.Point;
+
+                    //read furniture set from the revit room
+                    string furnitureSetModelItem = Utils.GetParameterValueAsString(room, "Furniture Set");
+                    if(furnitureSetModelItem != null && furnitureSetModelItem != "-")
+                    {
+                        foreach (string[] fSet in furnitureSetList)
+                        {
+                            //check if the furniture set in list matching with room furniture set in model
+                            if (fSet[0] == furnitureSetModelItem)
+                            {
+                                //get furnitures from furniture set list
+                                furnitureSetListItems = fSet[2];
+
+                                //split furniture list
+                                tempFurnitureListArray = furnitureSetListItems.Split(',');
+
+                                //trim spaces
+                                foreach (string fl in tempFurnitureListArray)
+                                {                                    
+                                    furnitureList.Add(fl.Trim());
+                                }
+
+                                //get items in the furniture list from furniture set list
+                                foreach (string furnitureName in furnitureList)
+                                {
+                                    //check for a matching furniture name
+                                    foreach (string[] furniture in furnitureTypesList)
+                                    {
+                                        if (furnitureName.Equals(furniture[0]))
+                                        {
+                                            revFamName = furniture[1];
+                                            revFamType = furniture[2];
+
+                                            //get family symbol
+                                            FamilySymbol famSym = Utils.GetFamilySymbolByName(doc,
+                                                revFamName, revFamType);
+
+                                            //place family
+                                            FamilyInstance newInstance = doc.Create.NewFamilyInstance(roomPoint, famSym,
+                                                StructuralType.NonStructural);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                tran.Commit();
             }
-
-            // Modify document within a transaction
-
-            using (Transaction tx = new Transaction(doc))
-            {
-                tx.Start("Transaction Name");
-                tx.Commit();
-            }
-
             return Result.Succeeded;
         }
+
+
     }
 }
---
+++ 05_Challenge/Utils.cs
@@ -0,0 +1,83 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+
+public static class Utils
+{
+    public static FamilySymbol GetFamilySymbolByName(Document doc, string familyName,
+        string familySymbolName)
+    {
+        //filter family symbols
+        FilteredElementCollector col = new FilteredElementCollector(doc);
+        col.OfClass(typeof(FamilySymbol));
+
+        //return matching family symbol
+        foreach(FamilySymbol fs in col)
+        {
+            //check for mathing name
+            if(fs.FamilyName == familyName && fs.Name == familySymbolName)
+            {
+                return fs;
+            }
+        }
+
+        return null;
+    }
+
+    public static List<string[]> GetFurnitureTypes()
+    {
+        List<string[]> returnList = new List<string[]>();
+        //returnList.Add(new string[] { "Furniture Name", "Revit Family Name", "Revit Family Type" });
+        returnList.Add(new string[] { "desk", "Desk", "60in x 30in" });
+        returnList.Add(new string[] { "task chair", "Chair-Task", "Chair-Task" });
+        returnList.Add(new string[] { "side chair", "Chair-Breuer", "Chair-Breuer" });
+        returnList.Add(new string[] { "bookcase", "Shelving", "96in x 12in x 84in" });
+        returnList.Add(new string[] { "loveseat", "Sofa", "54in" });
+        returnList.Add(new string[] { "teacher desk", "Table-Rectangular", "48in x 30in" });
+        returnList.Add(new string[] { "student desk", "Desk", "60in x 30in Student" });
+        returnList.Add(new string[] { "computer desk", "Table-Rectangular", "48in x 30in" });
+        returnList.Add(new string[] { "lab desk", "Table-Rectangular", "72in x 30in" });
+        returnList.Add(new string[] { "lounge chair", "Chair-Corbu", "Chair-Corbu" });
+        returnList.Add(new string[] { "coffee table", "Table-Coffee", "30in x 60in x 18in" });
+        returnList.Add(new string[] { "sofa", "Sofa-Corbu", "Sofa-Corbu" });
+        returnList.Add(new string[] { "dining table", "Table-Dining", "30in x 84in x 22in" });
+        returnList.Add(new string[] { "dining chair", "Chair-Breuer", "Chair-Breuer" });
+        returnList.Add(new string[] { "stool", "Chair-Task", "Chair-Task" });
+
+        return returnList;
+    }
+
+    public static List<string[]> GetFurnitureSets()
+    {
+        List<string[]> returnList = new List<string[]>();
+        //returnList.Add(new string[] { "Furniture Set", "Room Type", "Included Furniture" });
+        returnList.Add(new string[] { "A", "Office", "desk, task chair, side chair, bookcase" });
+        returnList.Add(new string[] { "A2", "Office", "desk, task chair, side chair, bookcase, loveseat" });
+        returnList.Add(new string[] { "B", "Classroom - Large", "teacher desk, task chair, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk" });
+        returnList.Add(new string[] { "B2", "Classroom - Medium", "teacher desk, task chair, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk" });
+        returnList.Add(new string[] { "C", "Computer Lab", "computer desk, computer desk, computer desk, computer desk, computer desk, computer desk, task chair, task chair, task chair, task chair, task chair, task chair" });
+        returnList.Add(new string[] { "D", "Lab", "teacher desk, task chair, lab desk, lab desk, lab desk, lab desk, lab desk, lab desk, lab desk, stool, stool, stool, stool, stool, stool, stool" });
+        returnList.Add(new string[] { "E", "Student Lounge", "lounge chair, lounge chair, lounge chair, sofa, coffee table, bookcase" });
+        returnList.Add(new string[] { "F", "Teacher's Lounge", "lounge chair, lounge chair, sofa, coffee table, dining table, dining chair, dining chair, dining chair, dining chair, bookcase" });
+        returnList.Add(new string[] { "G", "Waiting Room", "lounge chair, lounge chair, sofa, coffee table" });
+
+        return returnList;
+    }
+
+    internal static string GetParameterValueAsString(Element curElement, string paramName)
+    {
+        //create return value variable
+        string returnValue = "";
+
+        //get parameter. returns a list
+        IList<Parameter> paramList = curElement.GetParameters(paramName);
+
+        //get the first element from the list
+        Parameter myPram = paramList[0];
+
+        //return value
+        returnValue = myPram.AsValueString();
+
+        return returnValue;
+    }
+}
